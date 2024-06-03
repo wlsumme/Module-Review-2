85485ee684eedb061203ffa9d80dcc4227dcd027
William Summe <wlsumme@gmail.com>
2024-06-03 12:04:32 -0500

    step 1

---
+++ ModuleReview222/Program.cs
@@ -0,0 +1,7 @@
+﻿using ModuleReview222;
+
+Student studentOne = new Student("William Summe", 85);
+Student studentTwo = new Student("Lauren Summe", 18);
+
+studentOne.Grades.Add(86.1);
+studentTwo.Grades.Add(56.2);
---
+++ ModuleReview222/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuleReview222
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int ID { get; set; }
+        public List<double> Grades { get; set; } = new List<double>();
+        public Student(string name, int id) 
+
+        {
+            Name = name;
+            ID = id;
+            
+        }
+
+        public void AddGrade(params double[] grades)
+        {
+            Grades.AddRange(grades);
+        }
+    }
+}
